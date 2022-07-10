4de27443adc2f6328491d11fbab09771048025f0
Oscar Gomez <oscarthedev@gmail.com>
2022-07-10 13:32:00 -0600

    [ADD] initial styling to timer component

--- components/timer.jsx
+++ components/timer.jsx
@@ -2,8 +2,9 @@ export default function timer() {
 	return (
 		<div className="timer">
 			<div className="timer_container">
-				<div className="timer_container_timer"></div>
-				<div className="timer_container_stop"></div>
+				<div className="timer_container_status">Working on "Current Task"</div>
+				<div className="timer_container_timer">25:00</div>
+				<div className="timer_container_stop">Stop</div>
 			</div>
 		</div>
 	)
