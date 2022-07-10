40d2cec7765ccf16abc818b32a10d7c9c16d3982
Oscar Gomez <oscarthedev@gmail.com>
2022-07-10 09:22:49 -0600

    [ADD] timer component

---
+++ components/timer.jsx
@@ -0,0 +1,10 @@
+export default function timer() {
+	return (
+		<div className="timer">
+			<div className="timer_container">
+				<div className="timer_container_timer"></div>
+				<div className="timer_container_stop"></div>
+			</div>
+		</div>
+	)
+}
