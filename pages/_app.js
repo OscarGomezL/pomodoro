0c7b00e0068730b3b5bc732b820b751a1019cde9
Oscar Gomez <oscarthedev@gmail.com>
2022-07-10 13:33:39 -0600

    [ADD] all styles imported to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,12 @@
+import '../styles/font-family.css'
+import '../styles/reset.css'
 import '../styles/global.css'
+import '../styles/var.css'
+import '../styles/header.css'
+import '../styles/main.css'
+import '../styles/timer.css'
+import '../styles/tasks.css'
+import '../styles/task.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
