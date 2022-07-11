ca2e1d5374cb36da03c784108a20cd52c8dfbb59
Oscar Gomez <oscarthedev@gmail.com>
2022-07-11 13:52:53 -0600

    [ADD] redux log state now being applied to tasks component

--- components/tasks.jsx
+++ components/tasks.jsx
@@ -1,20 +1,51 @@
+import {useState, useEffect} from 'react'
 import Task from "./task"
-import { add } from "../logic/swal"
+import Swal2 from "sweetalert2"
+
+import { logger } from '../redux/actions'
+import { useDispatch, useSelector } from "react-redux"
+
 export default function tasks() {
-	let theTasks = [
-		{
-			name: "DSA",
-			notes: ["remember toooo", "lalala"],
-			status: "on process",
-			done: 0,
-			mustDo: 2,
-		},
-	]
+	let log = useSelector(s=>s.log)
+	const [tasks, setTasks] = useState()
+	const dispatch = useDispatch()
+	useEffect(() => {
+		setTasks(log ? log.tasks : [])
+	}, [log]);
+	const add = () => {
+		Swal2.fire({
+			title: "Add a Task",
+			text: 'Try clicking the "My Account" button',
+			html: `
+				<form autocomplete="off">
+					<input id="login-swal-input-1" type="text" placeholder="What Are You Working On?" class="swal2-input" required="true"/>
+				</form>
+			`,
+			color:"var(--red2)",
+			background: "var(--red3)",
+			confirmButtonColor: "var(--red2)",
+			preConfirm: function() {
+				return new Promise((res,rej) =>{
+					res({
+						name: document.getElementById(`login-swal-input-1`).value
+					})
+				})
+			}
+		})
+		.then(s=>{
+			let user = JSON.parse(localStorage.getItem('User'))
+			user.tasks.push({name:s.value.name, notes: [], status:"", done: 0, mustDo:1})
+			localStorage.setItem('User', JSON.stringify(user))
+			dispatch(logger('PATCH', user))
+		})
+		.catch(console.log)
+	}
+	if(tasks === undefined) return null
 	return (
 		<div className="tasks">
 			<div className="tasks_title">Tasks</div>
 			<div className="tasks_container">
-				{theTasks.map(el=> <Task name={el.name} notes={el.notes} status={el.status} done={el.done} mustDo={el.mustDo}/>)}
+				{tasks.map(el=> <Task name={el.name} notes={el.notes} status={el.status} done={el.done} mustDo={el.mustDo}/>)}
 				<div 
 					className="tasks_container_add-task"
 					onClick={()=>add()}
