7a73e851f1c2b8f14ad41a37dcf34e1502b17cc6
Oscar Gomez <oscarthedev@gmail.com>
2022-07-12 23:09:39 -0600

    [ADD] changeTaskName function to logic swal js file

--- logic/swal.js
+++ logic/swal.js
@@ -6,10 +6,20 @@ export const deleteTask = (id) => {
 	Swal2.fire(
 		{title: 'Are you sure you want to delete this task?',
 		icon: 'warning',
+		color:"var(--red4)",
+		background: "var(--red3)",
+		iconColor: "var(--red4)",
+		confirmButtonColor: "var(--red4)",
+		confirmButtonText: 'Delete'}
+	)
+}
+export const changeTaskName = (id) => {
+	let Swal2 = withReactContent(swalLibrary)
+	Swal2.fire(
+		{title: 'Change the name of this task',
 		color:"var(--red2)",
 		background: "var(--red3)",
 		confirmButtonColor: "var(--red2)",
 		confirmButtonText: 'Delete'}
 	)
- 
 }
